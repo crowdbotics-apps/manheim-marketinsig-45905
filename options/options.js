11c10f4f8b7ed412ab182986833a733cc66fb18f
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:43:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "manheim-marketinsig-45905",
+  url: "https://manheim-marketinsig-45905.botics.co",
+  api: "https://manheim-marketinsig-45905.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
